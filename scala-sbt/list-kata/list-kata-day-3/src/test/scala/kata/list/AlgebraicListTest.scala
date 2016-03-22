5dca394d4d42c4ffefffc30c52f123f5faecff06
Alex-Diez <alexvic.amatory@gmail.com>
2016-03-22 09:46:23 +0200

    algebraic list kata third day

---
+++ scala-sbt/list-kata/list-kata-day-3/src/test/scala/kata/list/AlgebraicListTest.scala
@@ -0,0 +1,33 @@
+package kata.list
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class AlgebraicListTest extends FlatSpec with Matchers {
+
+    it should " create a new empty list " in {
+        val list = new AlgebraicList()
+
+        list.size shouldBe 0
+        list.isEmpty shouldBe true
+    }
+
+    it should " add elements at the front of a list " in {
+        val list = new AlgebraicList()
+
+        val l = 3 :: 2 :: 1 :: list
+
+        l.size shouldBe 3
+        l(0) shouldBe 3
+        l(1) shouldBe 2
+        l(2) shouldBe 1
+    }
+
+    it should " concatenate two lists " in {
+        val listOne = 3 :: 2 :: 1 :: new AlgebraicList()
+        val listTwo = 6 :: 5 :: 4 :: new AlgebraicList()
+
+        val concat = listTwo ::: listOne
+
+        concat shouldBe 6 :: 5 :: 4 :: 3 :: 2 :: 1 :: new AlgebraicList()
+    } 
+}
