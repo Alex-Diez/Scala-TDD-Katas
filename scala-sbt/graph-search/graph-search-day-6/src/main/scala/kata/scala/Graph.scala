13dca2aa924245dbce0519ae1452c607805ed7af
Alex-Diez <alexvic.amatory@gmail.com>
2016-05-10 10:20:25 +0300

    graph search kata sixth day

---
+++ scala-sbt/graph-search/graph-search-day-6/src/main/scala/kata/scala/Graph.scala
@@ -0,0 +1,25 @@
+package kata.scala
+
+import scala.collection.mutable
+
+class Graph {
+    private val edges = mutable.Map[Int, mutable.ArrayBuffer[Int]]()
+
+    def adjacentTo(vertex: Int): Iterator[Int] = {
+        edges(vertex).iterator
+    }
+
+    def contains(vertex: Int) = edges.contains(vertex)
+
+    def addEdge(from: Int, to: Int) = {
+        if (!edges.contains(from)) {
+            edges += (from -> new mutable.ArrayBuffer[Int]())
+        }
+        edges(from) += to
+        if (!edges.contains(to)) {
+            edges += (to -> new mutable.ArrayBuffer[Int]())
+        }
+        edges(to) += from
+    }
+
+}
---
+++ scala-sbt/graph-search/graph-search-day-6/src/main/scala/kata/scala/GraphSearch.scala
@@ -0,0 +1,48 @@
+package kata.scala
+
+import scala.collection.mutable
+
+class GraphSearch(private val marked: Set[Int]) {
+    def hasPathTo(vertex: Int): Boolean = marked.contains(vertex)
+
+}
+
+object DepthFirstSearch {
+    def create(graph: Graph, startVertex: Int): Option[GraphSearch] = {
+        if (!graph.contains(startVertex)) None
+        else {
+            def depthFirstSearch(vertex: Int, marked: Set[Int]): Set[Int] = {
+                graph.adjacentTo(vertex)
+                        .filterNot(v => marked.contains(v))
+                        .foldLeft(marked + vertex)((marked, v) => depthFirstSearch(v, marked))
+            }
+            Some(new GraphSearch(depthFirstSearch(startVertex, Set())))
+        }
+    }
+
+}
+
+object BreadthFirstSearch {
+    def create(graph: Graph, startVertex: Int):Option[GraphSearch] = {
+        if (!graph.contains(startVertex)) None
+        else {
+            def breadthFirstSearch(vertex: Int): Set[Int] = {
+                val queue = mutable.Queue[Int]() += vertex
+                val marked = mutable.Set[Int]() += vertex
+                while (queue.nonEmpty) {
+                    val v = queue.dequeue()
+                    graph.adjacentTo(v)
+                            .filterNot(v => marked.contains(v))
+                            .foreach(
+                                v => {
+                                    marked += v
+                                    queue += v
+                                }
+                            )
+                }
+                Set() ++ marked
+            }
+            Some(new GraphSearch(breadthFirstSearch(startVertex)))
+        }
+    }
+}
